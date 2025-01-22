95cf7312c2d00f28d44f0c5b366b6ab686907446
Ravindra Devrani <ravindra.devrani003@gmail.com>
2025-01-22 20:10:30 +0530

    feat: book repo is migrated to dapper

--- BookShoppingCartMvcUI/Repositories/BookRepository.cs
+++ BookShoppingCartMvcUI/Repositories/BookRepository.cs
@@ -3,42 +3,41 @@ using Dapper;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 
-namespace BookShoppingCartMvcUI.Repositories
+namespace BookShoppingCartMvcUI.Repositories;
+public interface IBookRepository
 {
-    public interface IBookRepository
+    Task AddBook(Book book);
+    Task DeleteBook(Book book);
+    Task<Book?> GetBookById(int id);
+    Task<IEnumerable<Book>> GetBooks();
+    Task UpdateBook(Book book);
+}
+
+public class BookRepository : IBookRepository
+{
+    private readonly IConfiguration _config;
+    private readonly string _constr;
+    public BookRepository(IConfiguration config)
     {
-        Task AddBook(Book book);
-        Task DeleteBook(Book book);
-        Task<Book?> GetBookById(int id);
-        Task<IEnumerable<Book>> GetBooks();
-        Task UpdateBook(Book book);
+        _config = config;
+        _constr = _config.GetConnectionString("DefaultConnection");
     }
 
-    public class BookRepository : IBookRepository
+    public async Task AddBook(Book book)
     {
-        private readonly IConfiguration _config;
-        private readonly string _constr;
-        public BookRepository(IConfiguration config)
-        {
-            _config = config;
-            _constr = _config.GetConnectionString("DefaultConnection");
-        }
-
-        public async Task AddBook(Book book)
-        {
-            IDbConnection connection = new SqlConnection(_constr);
-            string sql = @"
+        IDbConnection connection = new SqlConnection(_constr);
+        string sql = @"
                 insert into
                 Book (BookName,AuthorName,Price,Image,GenreId)
                 values (@BookName,@AuthorName,@Price,@Image,@GenreId);
             ";
-            await connection.ExecuteAsync(sql);
-        }
+        await connection.ExecuteAsync(sql, book);
+    }
 
-        public async Task UpdateBook(Book book)
-        {
-            IDbConnection connection = new SqlConnection(_constr);
-            string sql = @"
+    public async Task UpdateBook(Book book)
+    {
+        IDbConnection connection = new SqlConnection(_constr);
+        string sql = @"
                update Book
                set 
                 BookName=@BookName,
@@ -46,18 +45,63 @@ namespace BookShoppingCartMvcUI.Repositories
                 Price=@Price,
                 Image=@Image,
                 GenreId=@GenreId
+               where Id=@Id
             ";
-            await connection.ExecuteAsync(sql, book);
-        }
-
-        public async Task DeleteBook(Book book)
-        {
-            _context.Books.Remove(book);
-            await _context.SaveChangesAsync();
-        }
-
-        public async Task<Book?> GetBookById(int id) => await _context.Books.FindAsync(id);
-
-        public async Task<IEnumerable<Book>> GetBooks() => await _context.Books.Include(a => a.Genre).ToListAsync();
+        await connection.ExecuteAsync(sql, book);
     }
+
+    public async Task DeleteBook(Book book)
+    {
+        IDbConnection connection = new SqlConnection(_constr);
+        string sql = @"
+                delete from 
+                Book
+                where Id=@Id 
+            ";
+        await connection.ExecuteAsync(sql, new { book.Id });
+    }
+
+    public async Task<Book?> GetBookById(int id)
+    {
+        IDbConnection connection = new SqlConnection(_constr);
+        string sql = @"
+             select
+               b.Id,
+               b.BookName,
+               b.AuthorName,
+               b.Price,
+               b.Image,
+               b.GenreId,
+               g.GenreName
+             from Book b
+             join Genre g
+             on b.GenreId = g.Id
+             where 
+                b.Id=@id
+            ";
+        Book? book = await connection.QueryFirstOrDefaultAsync<Book>(sql, new { id });
+        return book;
+    }
+
+    public async Task<IEnumerable<Book>> GetBooks()
+    {
+        IDbConnection connection = new SqlConnection(_constr);
+        string sql = @"
+             select
+               b.Id,
+               b.BookName,
+               b.AuthorName,
+               b.Price,
+               b.Image,
+               b.GenreId,
+               g.GenreName
+             from Book b
+             join Genre g
+             on b.GenreId = g.Id
+             order by 
+               b.BookName asc
+            ";
+        return await connection.QueryAsync<Book>(sql);
+    }
+
 }
